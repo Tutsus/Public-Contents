468f8d26b60d99dad447b8bacebca592ea870d28
Tutsus <nevertrollxd@gmail.com>
2023-08-06 23:10:53 +0700

    new file:   Email-Submit/.eslintrc.cjs 	new file:   Email-Submit/.gitignore

---
+++ Email-Submit/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import Emailform from './Email-form';
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <Emailform/>
+    </>
+  )
+}
+
+export default App
---
+++ Email-Submit/src/Email-form.jsx
@@ -0,0 +1,11 @@
+export default function Emailform() {
+    return (
+        <nav className=" bg-blue-100 max-w-screen h-screen flex justify-center items-center">
+            <div className="bg-white">
+                <div>
+                    <h1>Hello</h1>
+                </div>
+            </div>
+        </nav>
+    )
+}
