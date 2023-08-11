7a5c04f0934cf5aeccb270bd35d469660860364e
Tutsus <nevertrollxd@gmail.com>
2023-08-11 08:24:00 +0700

    renamed:    age-calculator-app-main/Style-Guide/.gitignore -> age-calculator-app-main/.gitignore

--- age-calculator-app-main/Age-Calculator/src/Age-Calculator.jsx
+++ age-calculator-app-main/Age-Calculator/src/Age-Calculator.jsx
@@ -1,7 +1,24 @@
 export default function Calculator() {
     return (
         <>
-        hello
+            <nav className="max-w-screen h-screen bg-black-100 flex justify-center items-center">
+                <div className="bg-white grid grid-rows-2">
+                    <div>
+                        <ul>
+                            <li>DD</li>
+                            <li>MM</li>
+                            <li>YYYY</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>DD</li>
+                            <li>MM</li>
+                            <li>YYYY</li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </>
     )
 }
