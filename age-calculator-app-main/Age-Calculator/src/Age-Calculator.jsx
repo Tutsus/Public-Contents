e18eb532905a12e26f1a4d6394040eed79c584c6
Tutsus <nevertrollxd@gmail.com>
2023-08-16 00:01:14 +0700

    modified:   age-calculator-app-main/Age-Calculator/src/Age-Calculator.jsx

--- age-calculator-app-main/Age-Calculator/src/Age-Calculator.jsx
+++ age-calculator-app-main/Age-Calculator/src/Age-Calculator.jsx
@@ -1,4 +1,28 @@
+import { useState } from "react";
 export default function Calculator() {
+
+    
+    const [day, setDay] = useState(false);
+    const [month, setMonth] = useState(false);
+    const [year, setYear] = useState(false);
+
+    function handleClick() {
+
+    }
+    
+    function handleValueDay(event) {
+        setDay(event.target.value);
+    }
+
+    function handleValueMonth(event) {
+        setMonth(event.target.value);
+    }
+
+    function handleValueYear(event) {
+        setYear(event.target.value);
+    }
+
+
     return (
         <>
             <nav className="max-w-screen h-screen bg-black-100 flex justify-center items-center">
@@ -9,19 +33,45 @@ export default function Calculator() {
                         <p className="mx-3 md:mx-7 w-24 sm:w-28 md:w-36 font-medium text-sm">Year</p>
                     </div>
                         <div className="flex">
-                            <input className="p-4 px-5 mx-3 md:mx-7 w-24 sm:w-28 md:w-36 h-18 rounded-xl text-xl sm:text-3xl font-bold border border-black-400 outline-purple-900 " type="number" placeholder="DD" name="DD" id="Day" min="1" max="31"/>
-                            <input className="p-4 px-5 mx-3 md:mx-7 w-24 sm:w-28 md:w-36 h-18 rounded-xl text-xl sm:text-3xl font-bold border border-black-400 outline-purple-900 " type="number" placeholder="MM" name="MM" id="Month" min="1" max="12"/>
-                            <input className="p-4 px-5 mx-3 md:mx-7 w-24 sm:w-28 md:w-36 h-18 rounded-xl text-xl sm:text-3xl font-bold border border-black-400 outline-purple-900 " type="number" placeholder="YYYY" name="YYYY" id="Year" min="1900"/>
+                            <input className="p-4 px-5 mx-3 md:mx-7 w-24 sm:w-28 md:w-36 h-18 rounded-xl text-xl sm:text-3xl font-bold border border-black-400 outline-purple-900 " 
+                                type="number" 
+                                placeholder="DD" 
+                                name="DD" 
+                                id="Day" 
+                                min="1" 
+                                max="31"
+                                onChange={handleValueDay}
+                                value={day}
+                            />
+                            <input className="p-4 px-5 mx-3 md:mx-7 w-24 sm:w-28 md:w-36 h-18 rounded-xl text-xl sm:text-3xl font-bold border border-black-400 outline-purple-900 " 
+                                type="number" 
+                                placeholder="MM" 
+                                name="MM" 
+                                id="Month" 
+                                min="1" 
+                                max="12"
+                                onChange={handleValueMonth}
+                                value={month}
+                            />
+                            <input className="p-4 px-5 mx-3 md:mx-7 w-24 sm:w-28 md:w-36 h-18 rounded-xl text-xl sm:text-3xl font-bold border border-black-400 outline-purple-900 " 
+                                type="number" 
+                                placeholder="YYYY" 
+                                name="YYYY" 
+                                id="Year" 
+                                min="1900"
+                                onChange={handleValueYear}
+                                value={year}
+                            />
                         </div>
                         <div className="flex justify-center items-center my-8 mt-12 md:mt-4 md:my-0">
                             <div className="border w-10/12 md:px-80"></div>
-                            <img className="p-4 rounded-full w-16 cursor-pointer bg-purple-700 hover:bg-black-950 transition-all ease-in-out duration-500" src="icon-arrow.svg" alt="Arrow-Icon" />
+                            <img onClick={handleClick} className="p-4 rounded-full w-16 cursor-pointer bg-purple-700 hover:bg-black-950 transition-all ease-in-out duration-500" src="icon-arrow.svg" alt="Arrow-Icon" />
                             <div className="md:hidden border w-10/12 md:px-80"></div>
                         </div>
                         <div className="ml-8">
-                            <h1 className=" font-bold text-6xl sm:text-8xl md:text-9xl">--years</h1>
-                            <h1 className=" font-bold text-6xl sm:text-8xl md:text-9xl">--months</h1>
-                            <h1 className=" font-bold text-6xl sm:text-8xl md:text-9xl">--days</h1>
+                            <h1 className=" font-bold text-6xl sm:text-8xl md:text-9xl">{year}years</h1>
+                            <h1 className=" font-bold text-6xl sm:text-8xl md:text-9xl">{month}months</h1>
+                            <h1 className=" font-bold text-6xl sm:text-8xl md:text-9xl">{day}days</h1>
                         </div>
                 </div>
             </nav>
