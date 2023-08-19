389997038f86b9912cc5ca226f78d8a803f36a09
Tutsus <nevertrollxd@gmail.com>
2023-08-19 22:54:08 +0700

    modified:   Compoments/age-calculator-app-main/Age-Calculator/src/Age-Calculator.jsx

--- Compoments/age-calculator-app-main/Age-Calculator/src/Age-Calculator.jsx
+++ Compoments/age-calculator-app-main/Age-Calculator/src/Age-Calculator.jsx
@@ -5,48 +5,18 @@ export default function Calculator() {
     const [day, setDay] = useState(false);
     const [month, setMonth] = useState(false);
     const [year, setYear] = useState(false);
-    const [invalidDay, setInvalidDay] = useState(true);
-    const [invalidMonth, setInvalidMonth] = useState(true);
-    const [invalidYear, setInvalidYear] = useState(true);
     const [outputDay, setOutputDay] = useState("--");  
     const [outputMonth, setOutputMonth] = useState("--"); 
     const [outputYear, setOutputYear] = useState("--");
+    const [invalidDay, setInvalidDay] = useState("");
+    const [invalidMonth, setInvalidMonth] = useState("");
+    const [invalidYear, setInvalidYear] = useState("");
 
     function handleClick() {
 
-        if (day < 1 || day > 31) {
-            setInvalidDay(invalidDay);
-        if (invalidDay) {
-            setInvalidDay('Must be a valid day');
-        } else {    
-            setInvalidDay('This field is required');}
-        } else {
-            setInvalidDay(!invalidDay);
-        }
-
-        if (month < 1 || month > 12) {
-            setInvalidMonth(invalidMonth);
-        if (invalidMonth) {
-            setInvalidMonth('Must be a valid Month');
-        } else {
-            setInvalidMonth('This field is required');}
-        } else {
-            setInvalidMonth(!invalidMonth);
-        }
-
-        if (year < 1900 || year > 2020) {
-            setInvalidYear(invalidYear);
-        if (invalidYear) {
-            setInvalidYear('Must be a valid Year');
-        } else {
-            setInvalidYear('This field is required');}
-        } else {
-            setInvalidYear(!invalidYear);
-        }
-
-        let current_day = parseInt(moment().format('Do'));
-        let current_month = parseInt(moment().format('M'));
-        let current_year = parseInt(moment().format('YYYY'));
+        let current_day = parseInt(moment().format("Do"));
+        let current_month = parseInt(moment().format("M"));
+        let current_year = parseInt(moment().format("YYYY"));
         
         let date = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
 
@@ -58,28 +28,68 @@ export default function Calculator() {
             current_year--;
             current_month = current_month + 12;
         }
-        
-        if (current_day - day === current_day) {
-            toString(setOutputDay(calculateDay));
-        }
-        if (current_month - month === current_month) {
-            toString(setOutputMonth(calculateMonth));
-        }
-        if (current_year - year === current_year) {
-            toString(setOutputYear('--'));
-        }
 
         let calculateDay = current_day - day;
         let calculateMonth = current_month - month;
-        let calculateyear = current_year - year;
+        let calculateYear = current_year - year;
 
-        setOutputDay(calculateDay);
-        setOutputMonth(calculateMonth);
-        setOutputYear(calculateyear);
+        if (isNaN(!calculateDay)) {
+            setInvalidDay("This field is required");
+        } else if (day === false) {
+            setInvalidDay("This field is required");
+        } else if (day >= 1 && day <= 31) {
+            setInvalidDay("");
+        } else if (day < 1 || day > 31) {
+            setInvalidDay("Must be a valid day");
+        } else {
+            setInvalidDay("This field is required")
+        }
+
+        if (isNaN(!calculateMonth)) {
+            setInvalidMonth("This field is required");
+        } else if (month === false) {
+            setInvalidMonth("This field is required");
+        } else if (month >= 1 && month <= 12) {
+            setInvalidMonth("");
+        } else if (month < 1 || month > 12) {
+            setInvalidMonth("Must be a valid month");
+        } else {
+            setInvalidMonth("This field is required")
+        }
+
+        if (isNaN(!calculateYear)) {
+            setInvalidYear("This field is required");
+        } else if (year === false) {
+            setInvalidYear("This field is required");
+        } else if (year >= 1 && year <= current_year) {
+            setInvalidYear("");
+        } else if (year < 1 || year >= current_year){
+            setInvalidYear("Must be in the past");
+        } else {
+            setInvalidYear("This field is required")
+        }
+
+        if (day >= 1 && day <= 31){
+            setOutputDay(calculateDay);
+        } else {
+            setOutputDay("--");
+        }
+
+        if (month >= 1 && month <= 12){
+            setOutputMonth(calculateMonth);
+        } else {
+            setOutputMonth("--");
+        }
+
+        if (year >= 1 && year <= current_year){
+            setOutputYear(calculateYear);
+        } else {
+            setOutputYear("--");
+        }
     }
 
     function handleValueDay(event) {
-        const inputValue = parseInt(event.target.value);
+        let inputValue = parseInt(event.target.value);
         setDay(inputValue);
     }
 
@@ -99,23 +109,8 @@ export default function Calculator() {
                 <div className="bg-white rounded-2xl rounded-br-5xl py-6 px-6 sm:py-10 sm:px-8">
                     <div className="flex items-center uppercase mb-1 text-black-400 tracking-widest">
                         <p className="mx-3 md:mx-7 w-24 sm:w-28 md:w-36 font-medium text-sm">Day</p>
-                        {invalidDay && (
-                            <div className=" text-red-500">
-                                {invalidDay}
-                            </div>
-                        )}
                         <p className="mx-3 md:mx-7 w-24 sm:w-28 md:w-36 font-medium text-sm">Month</p>
-                        {invalidMonth && (
-                            <div className=" text-red-500">
-                                {invalidMonth}
-                            </div>
-                        )}
                         <p className="mx-3 md:mx-7 w-24 sm:w-28 md:w-36 font-medium text-sm">Year</p>
-                        {invalidYear && (
-                            <div className=" text-red-500">
-                                {invalidYear}
-                            </div>
-                        )}
                     </div>
                         <div className="flex">
                             <input className="p-4 px-5 mx-3 md:mx-7 w-24 sm:w-28 md:w-36 h-18 rounded-xl text-xl sm:text-3xl font-bold border border-black-400 outline-purple-900 " 
@@ -148,15 +143,44 @@ export default function Calculator() {
                                 value={year}
                             />
                         </div>
+                        <div className="flex">
+                            {invalidDay && (
+                                <div className=" md:mx-7 sm:w-28 md:w-36 font-medium text-sm text-red-500">
+                                    {invalidDay}
+                                </div>
+                            )}
+                            {invalidMonth && (
+                                <div className=" md:mx-7 sm:w-28 md:w-36 font-medium text-sm text-red-500">
+                                    {invalidMonth}
+                                </div>
+                            )}
+                            {invalidYear && (
+                                <div className="md:mx-7 sm:w-28 md:w-36 font-medium text-sm text-red-500">
+                                    {invalidYear}
+                                </div>
+                            )}
+                        </div>
                         <div className="flex justify-center items-center my-8 mt-12 md:mt-4 md:my-0">
                             <div className="border w-10/12 md:px-80"></div>
                             <img onClick={handleClick} className="p-4 rounded-full w-16 cursor-pointer bg-purple-700 hover:bg-black-950 transition-all ease-in-out duration-500" src="icon-arrow.svg" alt="Arrow-Icon" />
                             <div className="md:hidden border w-10/12 md:px-80"></div>
                         </div>
                         <div className="ml-8">
-                            <h1 className="font-bold text-6xl sm:text-8xl md:text-9xl">{outputYear}years</h1>
-                            <h1 className="font-bold text-6xl sm:text-8xl md:text-9xl">{outputMonth}months</h1>
-                            <h1 className="font-bold text-6xl sm:text-8xl md:text-9xl">{outputDay}days</h1>
+                            <h1 className="font-bold text-6xl sm:text-8xl md:text-9xl">
+                                {<span className="text-purple-700">
+                                    {isNaN(outputYear) ? "--" : outputYear}
+                                </span>}years
+                            </h1>
+                            <h1 className="font-bold text-6xl sm:text-8xl md:text-9xl">
+                                {<span className="text-purple-700">
+                                    {isNaN(outputMonth) ? "--" : outputMonth}
+                                </span>}months
+                            </h1>
+                            <h1 className="font-bold text-6xl sm:text-8xl md:text-9xl">
+                                {<span className="text-purple-700">
+                                    {isNaN(outputDay) ? "--" : outputDay}
+                                </span>}days
+                            </h1>
                         </div>
                 </div>
             </nav>
